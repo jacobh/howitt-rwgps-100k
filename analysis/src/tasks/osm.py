c26c62ae4282e8ba9f98bd3e48719f86d1703a26
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-03-18 19:21:03 +1100

    wip

--- analysis/src/tasks/osm.py
+++ analysis/src/tasks/osm.py
@@ -3,6 +3,8 @@ import geojson
 from prefect import task
 import numpy as np
 from typing import List
+import itertools
+from prefect.utilities.annotations import quote
 
 # from prefect.cache_policies import TASK_SOURCE, INPUTS
 
@@ -14,14 +16,29 @@ def load_highway_data(file_path) -> geojson.FeatureCollection:
         data = json.loads(content)
         return data
 
-@task
+
 def convert_highway_geometry_to_numpy(highway_feature: geojson.Feature) -> np.ndarray:
     """Convert highway geometry to numpy arrays"""
     
-    return np.ndarray(list(*geojson.coords(highway_feature)))
+    return np.array(list(geojson.coords(highway_feature)))
 
 @task
-def convert_highway_features_to_numpy(highway_features: List[geojson.Feature]):
+def convert_batch_highway_features_to_numpy(highway_features: List[geojson.Feature]) -> List[np.ndarray]:
+    return [convert_highway_geometry_to_numpy(highway_feature) for highway_feature in highway_features]
+
+@task
+def convert_all_highway_features_to_numpy(highway_features: List[geojson.Feature]) -> List[np.ndarray]:
     """Convert highway geometry to numpy arrays"""
 
-    return [convert_highway_geometry_to_numpy(highway_feature) for highway_feature in highway_features]
+    batch_size = 1000
+    
+    # Split into batches
+    batches = [highway_features[i:i+batch_size] for i in range(0, len(highway_features), batch_size)]
+
+    print(f"batches: {len(batches)}")
+
+    futures = convert_batch_highway_features_to_numpy.map(quote(batches))
+    
+
+    results = [future.result() for future in futures]
+    return list(itertools.chain(*results))
