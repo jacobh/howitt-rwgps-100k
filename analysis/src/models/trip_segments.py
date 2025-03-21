9d57cafaf911092bc33a50298dee5d543147e6b5
Jacob Haslehurst <jacob.haslehurst@amber.com.au>
2025-03-21 19:39:31 +1100

    type TripSegmentDimensions

--- analysis/src/models/trip_segments.py
+++ analysis/src/models/trip_segments.py
@@ -1,5 +1,6 @@
 from typing import List
 from pydantic import BaseModel
+from .rwgps import ActivityType
 
 
 class TripSegmentIndex(BaseModel):
@@ -10,4 +11,22 @@ class TripSegmentIndex(BaseModel):
 class TripSegmentIndexes(BaseModel):
     trip_id: int
     segments: List[TripSegmentIndex]
-    
+
+
+class TripSegmentDimensions(BaseModel):
+    user_id: int
+    trip_id: int
+    activity_type: ActivityType
+    elevation_gain_m: List[float]
+    elevation_loss_m: List[float]
+    offset_x: List[float]
+    offset_y: List[float]
+    distance_m: List[float]
+    elapsed_time_secs: List[float]
+    moving_time_secs: List[float]
+    matched_highway_idx: List[int]
+    matched_boundary_idxs: List[List[int]]
+    mean_heart_rate_bpm: List[float]
+    mean_temperature_c: List[float]
+    mean_cadence_rpm: List[float]
+    mean_power_w: List[float]
